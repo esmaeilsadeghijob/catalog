30287c7ea18284d21b28c6d8a2708f85f1094bb9
esmaeilsadeghijob <esmaeilsadeghijob@gmail.com>
2025-05-07 06:44:42 -0700

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { GlobalOutlined, InstagramOutlined, LinkedinOutlined } from '@ant-design/icons';
+import {GlobalOutlined, InstagramOutlined, LinkedinOutlined, WhatsAppOutlined} from '@ant-design/icons';
 
 const App: React.FC = () => {
     const text = "د کتر اسماعیل صادقی هفشجانی";
@@ -9,6 +9,7 @@ const App: React.FC = () => {
     const [showInstagram, setShowInstagram] = useState<boolean>(false);
     const [showLinkedin, setShowLinkedin] = useState<boolean>(false);
     const [showWebsite, setShowWebsite] = useState<boolean>(false);
+    const [showWhatsapp, setShowWhatsapp] = useState<boolean>(false);
 
     useEffect(() => {
         let index = 0;
@@ -22,6 +23,7 @@ const App: React.FC = () => {
                 setTimeout(() => setShowInstagram(true), 1000);
                 setTimeout(() => setShowLinkedin(true), 1500);
                 setTimeout(() => setShowWebsite(true), 2000); // نمایش وب‌سایت بعد از لینکدین
+                setTimeout(() => setShowWhatsapp(true), 2500); // نمایش وب‌سایت بعد از لینکدین
             }
         }, 120);
         return () => clearInterval(interval);
@@ -73,6 +75,17 @@ const App: React.FC = () => {
                            rel="noopener noreferrer"
                            className="social-link">
                             www.esmaeilsadeghi.ir
+                        </a>
+                    </div>
+                )}
+                {showWhatsapp && (
+                    <div className="whatsapp-container">
+                        <WhatsAppOutlined className="whatsapp-icon"/>
+                        <a href="https://wa.me/989122273119"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                           className="social-link">
+                            WhatsApp
                         </a>
                     </div>
                 )}
